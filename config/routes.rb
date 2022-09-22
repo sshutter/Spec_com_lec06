cc718e27c9f27217da4cc690c1305ee5b237e7d7
sshutter <6331335921@student.chula.ac.th>
2022-09-22 14:53:17 +0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :scores
+  resources :students
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'students/:id/edit_score', to: 'students#edit_score', as: 'student_edit_score'
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
